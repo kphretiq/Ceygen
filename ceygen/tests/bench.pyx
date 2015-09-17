436f058e61839a003f21d53bb6725a215235e8ef
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2015-09-17 18:31:02 +0200

    FIX: aparently, prange cannot run inside a context manager.
    Also using print function for Py3 compatibility.

--- ceygen/tests/bench.pyx
+++ ceygen/tests/bench.pyx
@@ -2,34 +2,33 @@
 # Copyright (c) 2013 Matěj Laitl <matej@laitl.cz>
 # Distributed under the terms of the GNU General Public License v2 or any
 # later version of the license, at your option.
-
+#cython: language=c++
+from __future__ import absolute_import, print_function
 from cython.parallel cimport prange
 
 import numpy as np
-cdef object np_dot = np.dot
-cdef object np_add = np.add
-cdef object np_multiply = np.multiply
-cdef object np_det = np.linalg.det
+cimport numpy as np
+
 
 import os
 import pickle
 import subprocess
 from time import time
 
-from support import CeygenTestCase, benchmark
-cimport ceygen.core as c
-cimport ceygen.elemwise as e
-cimport ceygen.lu as lu
+from .support import CeygenTestCase, benchmark
+from .. cimport core as c
+from .. cimport elemwise as e
+from .. cimport lu
 
 
-class timeit:
+class timeit(object):
     """Simple context manager to time interations of the block inside"""
-
     def __init__(self, func, implementation, args):
         self.func, self.iterations, self.cost = func, args['iterations'], args['cost']
         self.stats = args['self'].stats
         self.percall = args['self'].percall
         self.execute = True
+
         if implementation.startswith('numpy') and 'BENCHMARK_NUMPY' not in os.environ:
             self.execute = False
         self.implementation = implementation.rjust(args['self'].align)
@@ -39,12 +38,14 @@ class timeit:
         return self
 
     def __exit__(self, exc_type, exc_value, tb):
+
         self.elapsed = time() - self.elapsed
+
         if self.execute:
             percall = self.elapsed/self.iterations
             gflops = self.cost/percall/10.**9
-            print "{0}: {1:.2e}s per call, {2:.3f}s total, {3:5.2f} GFLOPS".format(
-                self.implementation, percall, self.elapsed, gflops)
+            print("{0}: {1:.2e}s per call, {2:.3f}s total, {3:5.2f} GFLOPS".format(
+                self.implementation, percall, self.elapsed, gflops))
             if isinstance(self.stats, dict):
                 key = self.func + '.' + self.implementation.strip()
                 if key not in self.stats:
@@ -54,6 +55,7 @@ class timeit:
                 self.percall[key].append(percall)
         else:
             assert self.elapsed < 0.01
+
         return False  # let the exceptions fall through
 
 
@@ -79,13 +81,13 @@ class Bench(CeygenTestCase):
                 filename += b'.pickle'
                 with open(filename, 'wb') as f:
                     pickle.dump({'sizes': self.sizes, 'stats': stats, 'percall': percall}, f)
-                    print "Saved stats to {0}".format(filename)
+                    print("Saved stats to {0}".format(filename))
 
     # core module
 
     @benchmark
     def test_bench_dot_vv(self):
-        print
+        print()
         cdef int iterations
         cdef double[:] x
 
@@ -95,7 +97,7 @@ class Bench(CeygenTestCase):
 
             cost = 2. * size
             iterations = min(1.0 * 10.**9 / cost, 1000000)
-            print "size: {0}, iterations: {1}".format(size, iterations)
+            print("size: {0}, iterations: {1}".format(size, iterations))
 
             with timeit(b"dot_vv", "ceygen", locals()) as context:
                 if context.execute:
@@ -117,12 +119,12 @@ class Bench(CeygenTestCase):
 
             cost = 2. * size**2.
             iterations = min(0.5 * 10.**9. / cost, 1000000)
-            print "size: {0}, iterations: {1}".format(size, iterations)
+            print("size: {0}, iterations: {1}".format(size, iterations))
 
             with timeit(b"dot_mv", "numpy", locals()) as context:
                 if context.execute:
                     for i in range(iterations):
-                        np_dot(x_np, y_np, out_np)
+                        np.dot(x_np, y_np, out_np)
             with timeit(b"dot_mv", "ceygen", locals()) as context:
                 if context.execute:
                     for i in range(iterations):
@@ -130,7 +132,7 @@ class Bench(CeygenTestCase):
 
     @benchmark
     def test_bench_dot_mv_noout(self):
-        print
+        print()
         cdef int iterations
         cdef double[:, :] x
         cdef double[:] y
@@ -142,12 +144,12 @@ class Bench(CeygenTestCase):
 
             cost = 2. * size**2.
             iterations = min(0.5 * 10.**9. / cost, 1000000)
-            print "size: {0}, iterations: {1}".format(size, iterations)
+            print("size: {0}, iterations: {1}".format(size, iterations))
 
             with timeit(b"dot_mv_noout", "numpy", locals()) as context:
                 if context.execute:
                     for i in range(iterations):
-                        np_dot(x_np, y_np)
+                        np.dot(x_np, y_np)
             with timeit(b"dot_mv_noout", "ceygen", locals()) as context:
                 if context.execute:
                     for i in range(iterations):
@@ -155,7 +157,7 @@ class Bench(CeygenTestCase):
 
     @benchmark
     def test_bench_dot_vm(self):
-        print
+        print()
         cdef int iterations
         cdef double[:] x, out
         cdef double[:, :] y
@@ -168,12 +170,12 @@ class Bench(CeygenTestCase):
 
             cost = 2. * size**2.
             iterations = min(0.5 * 10.**9. / cost, 1000000)
-            print "size: {0}, iterations: {1}".format(size, iterations)
+            print("size: {0}, iterations: {1}".format(size, iterations))
 
             with timeit(b"dot_vm", "numpy", locals()) as context:
                 if context.execute:
                     for i in range(iterations):
-                        np_dot(x_np, y_np, out_np)
+                        np.dot(x_np, y_np, out_np)
             with timeit(b"dot_vm", "ceygen", locals()) as context:
                 if context.execute:
                     for i in range(iterations):
@@ -181,7 +183,7 @@ class Bench(CeygenTestCase):
 
     @benchmark
     def test_bench_dot_mm(self):
-        print
+        print()
         cdef int iterations
         cdef double[:, :] x, y, out
 
@@ -193,12 +195,12 @@ class Bench(CeygenTestCase):
 
             cost = 2. * size**3.
             iterations = min(max(2. * 10.**9. / cost, 1), 1000000)
-            print "size: {0}*{0}, iterations: {1}".format(size, iterations)
+            print("size: {0}*{0}, iterations: {1}".format(size, iterations))
 
             with timeit(b"dot_mm", "numpy", locals()) as context:
                 if context.execute:
                     for i in range(iterations):
-                        np_dot(x_np, y_np, out_np)
+                        np.dot(x_np, y_np, out_np)
             with timeit(b"dot_mm", "ceygen", locals()) as context:
                 if context.execute:
                     for i in range(iterations):
@@ -208,7 +210,7 @@ class Bench(CeygenTestCase):
 
     @benchmark
     def test_bench_multiply_vs(self):
-        print
+        print()
         cdef int iterations
         cdef double[:] x, out
 
@@ -219,7 +221,7 @@ class Bench(CeygenTestCase):
 
             cost = size
             iterations = min(0.25 * 10.**9 / cost, 1000000)
-            print "size: {0}, iterations: {1}".format(size, iterations)
+            print("size: {0}, iterations: {1}".format(size, iterations))
 
             with timeit(b"multiply_vs", "ceygen", locals()) as context:
                 if context.execute:
@@ -228,7 +230,7 @@ class Bench(CeygenTestCase):
 
     @benchmark
     def test_bench_add_vv(self):
-        print
+        print()
         cdef int iterations
         cdef double[:] x, out
 
@@ -239,12 +241,12 @@ class Bench(CeygenTestCase):
 
             cost = size
             iterations = min(0.25 * 10.**9 / cost, 1000000)
-            print "size: {0}, iterations: {1}".format(size, iterations)
+            print("size: {0}, iterations: {1}".format(size, iterations))
 
             with timeit(b"add_vv", "numpy", locals()) as context:
                 if context.execute:
                     for i in range(iterations):
-                        np_add(x_np, x_np, out_np)
+                        np.add(x_np, x_np, out_np)
             with timeit(b"add_vv", "ceygen", locals()) as context:
                 if context.execute:
                     for i in range(iterations):
@@ -252,7 +254,7 @@ class Bench(CeygenTestCase):
 
     @benchmark
     def test_bench_add_ms(self):
-        print
+        print()
         cdef int iterations
         cdef double[:, :] x, out
 
@@ -263,7 +265,7 @@ class Bench(CeygenTestCase):
 
             cost = size**2.
             iterations = min(0.25 * 10.**9 / cost, 1000000)
-            print "size: {0}*{0}, iterations: {1}".format(size, iterations)
+            print("size: {0}*{0}, iterations: {1}".format(size, iterations))
 
             with timeit(b"add_ms", "ceygen", locals()) as context:
                 if context.execute:
@@ -272,7 +274,7 @@ class Bench(CeygenTestCase):
 
     @benchmark
     def test_bench_multiply_mm(self):
-        print
+        print()
         cdef int iterations
         cdef double[:, :] x, out
 
@@ -283,12 +285,12 @@ class Bench(CeygenTestCase):
 
             cost = size**2.
             iterations = min(0.25 * 10.**9 / cost, 1000000)
-            print "size: {0}*{0}, iterations: {1}".format(size, iterations)
+            print("size: {0}*{0}, iterations: {1}".format(size, iterations))
 
             with timeit(b"multiply_mm", "numpy", locals()) as context:
                 if context.execute:
                     for i in range(iterations):
-                        np_multiply(x_np, x_np, out_np)
+                        np.multiply(x_np, x_np, out_np)
             with timeit(b"multiply_mm", "ceygen", locals()) as context:
                 if context.execute:
                     for i in range(iterations):
@@ -298,7 +300,7 @@ class Bench(CeygenTestCase):
 
     @benchmark
     def test_bench_inv(self):
-        print
+        print()
         cdef int iterations
         cdef double[:, :] x, out
 
@@ -309,7 +311,7 @@ class Bench(CeygenTestCase):
 
             cost = size**3.  # 2/3 * n^3 floating point operations, but additional logic operations
             iterations = min(max(0.25 * 10.**9. / cost, 1), 1000000)
-            print "size: {0}*{0}, iterations: {1}".format(size, iterations)
+            print("size: {0}*{0}, iterations: {1}".format(size, iterations))
 
             with timeit(b"inv", "ceygen", locals()) as context:
                 if context.execute:
@@ -318,7 +320,7 @@ class Bench(CeygenTestCase):
 
     @benchmark
     def test_bench_iinv(self):
-        print
+        print()
         cdef int iterations
         cdef double[:, :] x
 
@@ -328,7 +330,7 @@ class Bench(CeygenTestCase):
 
             cost = size**3.  # 2/3 * n^3 floating point operations, but additional logic operations
             iterations = min(max(0.25 * 10.**9. / cost, 1), 1000000)
-            print "size: {0}*{0}, iterations: {1}".format(size, iterations)
+            print("size: {0}*{0}, iterations: {1}".format(size, iterations))
 
             with timeit(b"iinv", "ceygen", locals()) as context:
                 if context.execute:
@@ -337,7 +339,7 @@ class Bench(CeygenTestCase):
 
     @benchmark
     def test_bench_det(self):
-        print
+        print()
         cdef int i, iterations
         cdef double[:, :] x
         from multiprocessing import cpu_count, Pool
@@ -348,14 +350,14 @@ class Bench(CeygenTestCase):
 
             cost = size**3.
             iterations = 4 * min(max(int(0.25 * 10.**9. / cost), 1), 250000)
-            print "size: {0}*{0}, iterations: {1}".format(size, iterations)
+            print("size: {0}*{0}, iterations: {1}".format(size, iterations))
             origalign = self.align
             self.align = 17
 
             with timeit(b"det", "numpy", locals()) as context:
                 if context.execute:
                     for i in range(iterations):
-                        np_det(x_np)
+                        np.det(x_np)
 
             with timeit(b"det", "ceygen", locals()) as context:
                 if context.execute:
@@ -365,11 +367,13 @@ class Bench(CeygenTestCase):
             with timeit(b"det", "numpy parallel", locals()) as context:
                 if context.execute:
                     pool = Pool(processes=cpu_count())  # TODO: actual number of cores
-                    pool.map(np_det, (x_np for i in range(iterations)))
+                    pool.map(np.det, (x_np for i in range(iterations)))
 
-            with timeit(b"det", "ceygen parallel", locals()) as context:
-                if context.execute:
-                    for i in prange(iterations, nogil=True):
-                        lu.det(x)
+            context = timeit(b"det", "ceygen parallel", locals())
+            context.__enter__()
+            for i in prange(iterations, nogil=True):
+                lu.det(x)
+            context.__exit()
 
             self.align = origalign
+
